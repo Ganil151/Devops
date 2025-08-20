8a32a4e5e5403314a6d4154d0face68c3a7afad2
GSmash151 <ganilsmash@gmail.com>
2025-08-20 01:29:41 -0400

    first commit

--- Terraform/Aws_Projects/Sis_Lessons/terra_jenkins_v1/spms-app/main.tf
+++ Terraform/Aws_Projects/Sis_Lessons/terra_jenkins_v1/spms-app/main.tf
@@ -1,10 +1,10 @@
 module "vpc" {
-  source       = "../modules/vpc"
-  project_name = var.project_name
-  vpc_cidr_block = var.subnet_cidr_block
-  subnet_cidr_block = var.subnet_cidr_block
-  enable_dns_support = var.enable_dns_support
-  enable_dns_hostnames = var.enable_dns_hostnames
+  source                  = "../modules/vpc"
+  project_name            = var.project_name
+  vpc_cidr_block          = var.subnet_cidr_block
+  subnet_cidr_block       = var.subnet_cidr_block
+  enable_dns_support      = var.enable_dns_support
+  enable_dns_hostnames    = var.enable_dns_hostnames
   map_public_ip_on_launch = var.map_public_ip_on_launch
 }
 
