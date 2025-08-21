acc60df715b568ca2957a56bfe9302c923686676
GSmash151 <ganilsmash@gmail.com>
2025-08-21 16:17:52 -0400

    ec2 complete in jenkins project

--- CICD/Jenkins/Youtube_Lessons/basic_v1/jenkins_server_v1/jenkins-server/main.tf
+++ CICD/Jenkins/Youtube_Lessons/basic_v1/jenkins_server_v1/jenkins-server/main.tf
@@ -1,21 +1,32 @@
 module "vpc" {
-  source       = "../modules/vpc"
-  vpc_id = var.vpc_id
-  project_name = var.project_name
-  vpc_cidr_block = var.subnet_cidr_block
-  subnet_cidr_block = var.subnet_cidr_block
-  enable_dns_support = var.enable_dns_support
-  enable_dns_hostnames = var.enable_dns_hostnames
+  source                  = "../modules/vpc"
+  vpc_id                  = var.vpc_id
+  project_name            = var.project_name
+  vpc_cidr_block          = var.subnet_cidr_block
+  subnet_cidr_block       = var.subnet_cidr_block
+  enable_dns_support      = var.enable_dns_support
+  enable_dns_hostnames    = var.enable_dns_hostnames
   map_public_ip_on_launch = var.map_public_ip_on_launch
 }
 
 module "security_group" {
-  source = "../modules/security_group"
-  vpc_id = module.vpc.vpc_id
+  source       = "../modules/security_group"
+  vpc_id       = module.vpc.vpc_id
   project_name = var.project_name
 }
 
 module "keys" {
-  source = "../modules/keys"
+  source   = "../modules/keys"
   key_name = var.key_name
+}
+
+module "ec2" {
+  source                      = "../modules/ec2"
+  ami                         = var.ami
+  key_name                    = var.key_name
+  project_name                = var.project_name
+  instance_type               = var.instance_type
+  subnet_id                   = module.vpc.subnet_id
+  user_data_replace_on_change = var.user_data_replace_on_change
+  vpc_security_group_id       = module.security_group.security_group_id
 }
---
+++ CICD/Jenkins/Youtube_Lessons/basic_v1/jenkins_server_v1/modules/ec2/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "jenkins-server" {
+  ami                         = var.ami
+  key_name                    = var.key_name
+  subnet_id                   = var.subnet_id
+  instance_type               = var.instance_type
+  vpc_security_group_ids      = [var.vpc_security_group_id]
+  user_data_replace_on_change = var.user_data_replace_on_change
+
+  tags = {
+    Name = "${var.project_name}"
+  }
+
+}
