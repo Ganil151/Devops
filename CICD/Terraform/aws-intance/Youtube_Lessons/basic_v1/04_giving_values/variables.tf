08fa48dfcad8da6b86ed1eb892afd5c1e15f4f38
GSmash151 <ganilsmash@gmail.com>
2025-08-21 04:42:11 -0400

    Revert "Reapply "Moved Terraform Project folder""
    This reverts commit aec70aa57320e0c9ef2966b05340bb30e335bedc.

--- CICD/Terraform/aws-intance/Youtube_Lessons/basic_v1/04_giving_values/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "filename" {
-  type = string
-  default = "default-value"
-}
-
-variable "api_key" {
-  type = string
-  default = "DEFAULT"
-} 
--- CICD/Terraform/aws-intance/Youtube_Lessons/basic_v2/README.md
+++
@@ -1,2 +0,0 @@
-link:https://youtu.be/ZP_vAbjfFMs?t=769
-link2:https://www.youtube.com/watch?v=lSZstrOJ614&list=PL184oVW5ERMCirZu6wRL2NmUENHixB4mt
