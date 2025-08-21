ea8e2efd6420da442ac70539ad984cbd3ba76205
GSmash151 <ganilsmash@gmail.com>
2025-08-21 12:43:45 -0400

    jenkins project vpc complete

---
+++ CICD/Jenkins/Youtube_Lessons/basic_v1/jenkins_server_v1/modules/vpc/variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_id" {}
+variable "project_name" {}
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block"{}
+variable "enable_dns_support" {}
+variable "enable_dns_hostnames" {}
+variable "map_public_ip_on_launch" {}
