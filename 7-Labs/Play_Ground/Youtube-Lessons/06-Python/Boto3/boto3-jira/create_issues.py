d83e94006f761ed368d5e9677273c699b36b53c0
Ganil151 <ganilbatistyan@gmail.com>
2026-02-02 08:06:18 -0400

    create python test files and boto3 project

---
+++ 7-Labs/Play_Ground/Youtube-Lessons/06-Python/Boto3/boto3-jira/create_issues.py
@@ -0,0 +1,82 @@
+# This code sample uses the 'requests' library:
+# http://docs.python-requests.org
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+url = "https://your-domain.atlassian.net/rest/api/3/issue"
+
+auth = HTTPBasicAuth("email@example.com", "<api_token>")
+
+headers = {
+  "Accept": "application/json",
+  "Content-Type": "application/json"
+}
+
+payload = json.dumps( {
+  "fields": {
+    "description": {
+      "content": [
+        {
+          "content": [
+            {
+              "text": "My first ticket created via the REST API",
+              "type": "text"
+            }
+          ],
+          "type": "paragraph"
+        }
+      ],
+      "type": "doc",
+      "version": 1
+    },
+    "fixVersions": [
+      {
+        "id": "10001"
+      }
+    ],
+    "issuetype": {
+      "id": "10000"
+    },
+    "labels": [
+      "bugfix",
+      "blitz_test"
+    ],
+    "parent": {
+      "key": "PROJ-123"
+    },
+    "priority": {
+      "id": "20000"
+    },
+    "project": {
+      "id": "10000"
+    },
+    "reporter": {
+      "id": "5b10a2844c20165700ede21g"
+    },
+    "security": {
+      "id": "10000"
+    },
+    "summary": "Main order flow broken",
+    "timetracking": {
+      "originalEstimate": "10",
+      "remainingEstimate": "5"
+    },
+    "versions": [
+      {
+        "id": "10000"
+      }
+    ]
+  },
+  "update": {}
+} )
+
+response = requests.request(
+   "POST",
+   url,
+   data=payload,
+   headers=headers,
+   auth=auth
+)
+
+print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
---
+++ 7-Labs/Play_Ground/Youtube-Lessons/06-Python/Boto3/boto3-jira/ytb.md
@@ -0,0 +1,1 @@
+https://youtu.be/tvdIGMahlNM?list=PLdpzxOOAlwvKwTyYNJCUwGPvql0TrsPgv
