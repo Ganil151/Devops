7ae4bed594fb35e23f6e31f8bc0b4479ca08480c
Ganil151 <ganilbatistyan@gmail.com>
2026-02-08 09:40:20 -0400

    refactor(Devops): Update main.tf
    Changes summary:
    - Modified: 1 files
      • 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf

--- 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf
+++ 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf
@@ -142,7 +142,9 @@ resource "aws_route" "secondary_to_primary" {
   provider = aws.secondary
   route_table_id = aws_route_table.secondary_route_table.id
   destination_cidr_block = aws_vpc.primary_vpc.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.primary_to_secondary
+  vpc_peering_connection_id = aws_vpc_peering_connection.secondary_to_primary.id
+}
+
 
 resource "aws_vpc_peering_connection_accepter" "secondary_to_primary" {
   provider = aws.secondary
