d6ea8ae28cb696e1d1735ebd3c57436eeaa47f88
Ganil151 <ganilbatistyan@gmail.com>
2026-02-08 09:38:32 -0400

    refactor(Devops): Update main.tf
    Changes summary:
    - Modified: 1 files
      • 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf

--- 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf
+++ 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf
@@ -145,6 +145,7 @@ resource "aws_vpc_peering_connection_accepter" "secondary_to_primary" {
   
   tags = {
     Name = "Secondary-to-Primary"
-    Environment
+    Environment = var.environment
+    
   } 
 }
