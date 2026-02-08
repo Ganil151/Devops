e164f9df937956c7ec713f0ba2a83fb8858cf8cf
Ganil151 <ganilbatistyan@gmail.com>
2026-02-08 10:12:10 -0400

    refactor(Devops): Update main.tf
    Changes summary:
    - Modified: 1 files
      • 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf

--- 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf
+++ 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf
@@ -250,10 +250,42 @@ resource "aws_security_group" "secondary_sg" {
 }
 
 
+# EC2 Instance in Primary VPC
 resource "aws_instance" "primary_instance" {
-  provider = aws.primary
-  ami = data.aws_ami.primary_ami.id
-  subnet_id = aws_subnet.primary_subnet.id
+  provider               = aws.primary
+  ami                    = data.aws_ami.primary_ami.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.primary_subnet.id
   vpc_security_group_ids = [aws_security_group.primary_sg.id]
-  key_name = var.primary_key_name
+  key_name               = var.primary_key_name
+
+  user_data = local.primary_user_data
+
+  tags = {
+    Name        = "Primary-VPC-Instance"
+    Environment = "Demo"
+    Region      = var.primary_region
+  }
+
+  depends_on = [aws_vpc_peering_connection_accepter.secondary_accepter]
+}
+
+# EC2 Instance in Secondary VPC
+resource "aws_instance" "secondary_instance" {
+  provider               = aws.secondary
+  ami                    = data.aws_ami.secondary_ami.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.secondary_subnet.id
+  vpc_security_group_ids = [aws_security_group.secondary_sg.id]
+  key_name               = var.secondary_key_name
+
+  user_data = local.secondary_user_data
+
+  tags = {
+    Name        = "Secondary-VPC-Instance"
+    Environment = "Demo"
+    Region      = var.secondary_region
+  }
+
+  depends_on = [aws_vpc_peering_connection_accepter.secondary_accepter]
 }
