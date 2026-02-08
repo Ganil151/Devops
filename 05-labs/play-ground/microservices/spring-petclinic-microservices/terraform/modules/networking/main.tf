51c59976f3a1fe906aefade5536404eacb5aef26
Ganil151 <ganilbatistyan@gmail.com>
2026-02-08 11:30:24 -0400

    refactor(Devops): Update workspace.json and 1 others
    Changes summary:
    - Modified: 1 files
      • .obsidian/workspace.json
    - Untracked: 1 files
      • 05-labs/play-ground/microservices/spring-petclinic-microservices/terraform/modules/networking/main.tf

---
+++ 05-labs/play-ground/microservices/spring-petclinic-microservices/terraform/modules/networking/main.tf
@@ -0,0 +1,95 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${var.environment}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.environment}-igw"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnets_cidrs)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnets_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                     = "${var.environment}-public-${count.index + 1}"
+    "kubernetes.io/role/elb" = "1"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets_cidrs)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnets_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name                              = "${var.environment}-private-${count.index + 1}"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+resource "aws_nat_gateway" "this" {
+  count         = 1
+  allocation_id = aws_eip.nat[0].id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "${var.environment}-nat"
+  }
+}
+
+resource "aws_eip" "nat" {
+  count  = 1
+  domain = "vpc"
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "${var.environment}-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this[0].id
+  }
+
+  tags = {
+    Name = "${var.environment}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets_cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
