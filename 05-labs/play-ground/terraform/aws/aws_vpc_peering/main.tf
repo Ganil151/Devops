068f3e7ed11d32305283632451ea16c5f4a8ce1e
Ganil151 <ganilbatistyan@gmail.com>
2026-02-08 08:01:37 -0400

    refactor(Devops): Update main.tf
    Changes summary:
    - Modified: 1 files
      • 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf

--- 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf
+++ 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf
@@ -1,6 +1,8 @@
 resource "aws_vpc" "primary_vpc" {
   cidr_block = var.primary_vpc_cidr
   provider = aws.primary
+  enable_dns_hostnames = true
+  enable_dns_support = true
   instance_tenancy ="default"
 
   tags = {
