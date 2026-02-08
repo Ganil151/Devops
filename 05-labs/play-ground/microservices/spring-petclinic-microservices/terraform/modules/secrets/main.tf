54a037bc8a311013111cce5673db7bf3acccf5c6
Ganil151 <ganilbatistyan@gmail.com>
2026-02-08 11:32:46 -0400

    refactor(Devops): Update workspace.json and 1 others
    Changes summary:
    - Modified: 1 files
      • .obsidian/workspace.json
    - Untracked: 1 files
      • 05-labs/play-ground/microservices/spring-petclinic-microservices/terraform/modules/secrets/

---
+++ 05-labs/play-ground/microservices/spring-petclinic-microservices/terraform/modules/secrets/main.tf
@@ -0,0 +1,16 @@
+resource "aws_secretsmanager_secret" "db_creds" {
+  name = "${var.environment}/petclinic/db"
+}
+
+# The version/value is usually created outside of TF or with a dummy value initially
+resource "aws_secretsmanager_secret_version" "db_creds" {
+  secret_id     = aws_secretsmanager_secret.db_creds.id
+  secret_string = jsonencode({
+    username = "admin"
+    password = "change_me"
+  })
+  
+  lifecycle {
+    ignore_changes = [secret_string]
+  }
+}
