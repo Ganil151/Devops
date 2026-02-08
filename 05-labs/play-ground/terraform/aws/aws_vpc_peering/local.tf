15a5be054d78dbfa3f400bf9c7d914bd10ddbf93
Ganil151 <ganilbatistyan@gmail.com>
2026-02-08 10:13:49 -0400

    refactor(Devops): Update local.tf
    Changes summary:
    - Modified: 1 files
      • 05-labs/play-ground/terraform/aws/aws_vpc_peering/local.tf

--- 05-labs/play-ground/terraform/aws/aws_vpc_peering/local.tf
+++ 05-labs/play-ground/terraform/aws/aws_vpc_peering/local.tf
@@ -19,7 +19,7 @@ locals {
     apt-get install -y apache2
     systemctl start apache2
     systemctl enable apache2
-    echo "<h1>Secondary VPC Instance - ${var.secondary_region}</h1>" > /var/www/html/index.html
+    echo "<h1>Secondary VPC Instance - ${var.secondary}</h1>" > /var/www/html/index.html
     echo "<p>Private IP: $(hostname -I)</p>" >> /var/www/html/index.html
   EOF
 }
