63b649dd5c338381283cc3e819700f825e7496d0
Ganil151 <ganilbatistyan@gmail.com>
2026-02-08 09:26:37 -0400

    refactor(Devops): Update main.tf
    Changes summary:
    - Modified: 1 files
      • 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf

--- 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf
+++ 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf
@@ -110,5 +110,6 @@ resource "aws_route_table_association" "secondary_rtb" {
 
 resource "aws_vpc_peering_connection" "primary_to_secondary" {
   provider = aws.primary
-  vpc_id = aws_vpc.primary_vpc
+  vpc_id = aws_vpc.primary_vpc.id
+  pe
 }
