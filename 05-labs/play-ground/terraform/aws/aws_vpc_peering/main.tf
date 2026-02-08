dffea977714a6ca47651fb2851633e841d239858
Ganil151 <ganilbatistyan@gmail.com>
2026-02-08 08:02:00 -0400

    refactor(Devops): Update main.tf
    Changes summary:
    - Modified: 1 files
      • 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf

--- 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf
+++ 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf
@@ -3,9 +3,8 @@ resource "aws_vpc" "primary_vpc" {
   provider = aws.primary
   enable_dns_hostnames = true
   enable_dns_support = true
-  instance_tenancy ="default"
 
   tags = {
-    Name = "main"
+    Name = ""
   }
 }
