eaef2eba027d1eee089d92abbfc58e1f50299d66
Ganil151 <ganilbatistyan@gmail.com>
2026-02-08 08:19:30 -0400

    refactor(Devops): Update main.tf
    Changes summary:
    - Modified: 1 files
      • 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf

--- 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf
+++ 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf
@@ -22,4 +22,10 @@ resource "aws_vpc" "secondary_vpc" {
 
 resource "aws_subnet" "primary_subnet" {
   vpc_id     = aws_vpc.primary_vpc.id
-  cidr_block = var.prima
+  cidr_block = var.primary_subnet_cidr
+  provider   = aws.primary
+
+  tags = {
+    Name = "Primary-Subnet-${var.primary}"
+  }
+} 
