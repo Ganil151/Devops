a87bd0c1feefccc4c97f30f8c304de20162dbe19
Ganil151 <ganilbatistyan@gmail.com>
2026-02-08 13:05:48 -0400

    refactor(Devops): Update backend.tf and 2 others
    Changes summary:
    - Deleted: 3 files
      • 05-labs/play-ground/microservices/spring-petclinic-microservices/terraform/environments/dev/backend.tf
      • 05-labs/play-ground/microservices/spring-petclinic-microservices/terraform/environments/prod/backend.tf
      • 05-labs/play-ground/microservices/spring-petclinic-microservices/terraform/environments/staging/backend.tf

--- 05-labs/play-ground/microservices/spring-petclinic-microservices/terraform/environments/staging/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "petclinic-terraform-state-staging"
-    key            = "staging/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"
-  }
-}
