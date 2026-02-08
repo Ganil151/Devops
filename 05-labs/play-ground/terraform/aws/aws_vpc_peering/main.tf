d7847c47f181af895f385d8e0b590b79874f21c6
Ganil151 <ganilbatistyan@gmail.com>
2026-02-08 09:50:57 -0400

    refactor(Devops): Update main.tf
    Changes summary:
    - Modified: 1 files
      • 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf

--- 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf
+++ 05-labs/play-ground/terraform/aws/aws_vpc_peering/main.tf
@@ -163,5 +163,5 @@ resource "aws_vpc_peering_connection_accepter" "secondary_to_primary" {
 
 resource "aws_security_group" "primary_sg" {
   provider = aws.primary
-  name = 
+  name = "primary-vpc-sg"
 }
