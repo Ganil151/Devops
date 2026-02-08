b81ac56b6806faeeef8d36cca9392dae1f4b7091
Ganil151 <ganilbatistyan@gmail.com>
2026-02-08 08:20:32 -0400

    refactor(Devops): Update variables.tf
    Changes summary:
    - Modified: 1 files
      • 05-labs/play-ground/terraform/aws/aws_vpc_peering/variables.tf

--- 05-labs/play-ground/terraform/aws/aws_vpc_peering/variables.tf
+++ 05-labs/play-ground/terraform/aws/aws_vpc_peering/variables.tf
@@ -22,3 +22,11 @@ variable "secondary_vpc_cidr" {
   default = "10.1.0.0/16"
 }
 
+variable "primary_subnet_cidr" {
+  default = "10.0.0.0/24"
+}
+
+variable "secondary_subnet_cidr" {
+  default = "10.1.0.0/24"
+}
+
