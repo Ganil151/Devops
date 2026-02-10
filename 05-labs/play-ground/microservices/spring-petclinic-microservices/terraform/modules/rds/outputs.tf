d4d5417b509abb8c4dbfea5c10bb63067666119d
Ganil151 <ganilbatistyan@gmail.com>
2026-02-10 07:20:12 -0400

    refactor(Devops): Update workspace.json and 2 others
    Changes summary:
    - Modified: 2 files
      • .obsidian/workspace.json
      • 05-labs/play-ground/microservices/spring-petclinic-microservices/terraform/modules/rds/outputs.tf
    - Untracked: 1 files
      • 05-labs/play-ground/microservices/spring-petclinic-microservices/terraform/modules/ec2/outputs.tf

--- 05-labs/play-ground/microservices/spring-petclinic-microservices/terraform/modules/rds/outputs.tf
+++ 05-labs/play-ground/microservices/spring-petclinic-microservices/terraform/modules/rds/outputs.tf
@@ -1,9 +1,11 @@
 output "db_instance_endpoint" {
-  description = "The connection endpoint"
-  value       = module.db.db_instance_endpoint
+  value = module.db.db_instance_endpoint
 }
 
 output "db_instance_identifier" {
-  description = "The RDS instance identifier"
-  value       = module.db.db_instance_identifier
+  value = module.db.db_instance_identifier
+}
+
+output "db_instance_username" {
+  value = module.db.db_instance_username
 }
