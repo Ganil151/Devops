f4102c79966e1493d0b09843a838ea4d442bc458
Ganil151 <ganilbatistyan@gmail.com>
2026-02-10 10:28:41 -0400

    refactor(Devops): Update main.tf and 1 others
    Changes summary:
    - Modified: 2 files
      • 05-labs/play-ground/microservices/spring-petclinic-microservices/terraform/modules/eks/main.tf
      • 05-labs/play-ground/microservices/spring-petclinic-microservices/terraform/modules/eks/variables.tf

--- 05-labs/play-ground/microservices/spring-petclinic-microservices/terraform/modules/eks/main.tf
+++ 05-labs/play-ground/microservices/spring-petclinic-microservices/terraform/modules/eks/main.tf
@@ -1,63 +1,88 @@
 resource "aws_eks_cluster" "this" {
-  name     = "${var.environment}-petclinic-cluster"
+  name     = "${var.environment}-cluster"
   role_arn = aws_iam_role.cluster.arn
   version  = var.cluster_version
 
   vpc_config {
-    subnet_ids              = var.private_subnet_ids
-    endpoint_private_access = true
-    endpoint_public_access  = true
-    security_group_ids      = [aws_security_group.cluster.id]
-  }
-
-  encryption_config {
-    resources = ["secrets"]
-    provider {
-      key_arn = aws_kms_key.eks.arn
-    }
+    subnet_ids = var.private_subnet_ids
   }
 
   depends_on = [
     aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
   ]
-
-  tags = var.tags
 }
 
-resource "aws_kms_key" "eks" {
-  description             = "KMS key for EKS cluster ${var.environment}"
-  deletion_window_in_days = 7
-  enable_key_rotation     = true
-  tags                    = var.tags
+resource "aws_iam_role" "cluster" {
+  name = "${var.environment}-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
 
-resource "aws_security_group" "cluster" {
-  name        = "${var.environment}-eks-cluster-sg"
-  description = "Security group for EKS cluster"
-  vpc_id      = var.vpc_id
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${var.environment}-nodes"
+  node_role_arn   = aws_iam_role.node_group.arn
+  subnet_ids      = var.private_subnet_ids
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 4
+    min_size     = 1
   }
 
-  tags = merge(
-    var.tags,
-    {
-      Name = "${var.environment}-eks-cluster-sg"
-    }
-  )
+  instance_types = ["t3.medium"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_group_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node_group_AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node_group_AmazonEC2ContainerRegistryReadOnly,
+  ]
 }
 
-# OIDC Provider for IRSA
-data "tls_certificate" "this" {
-  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
+resource "aws_iam_role" "node_group" {
+  name = "${var.environment}-eks-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
 
-resource "aws_iam_openid_connect_provider" "this" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.this.certificates[0].sha1_fingerprint]
-  url             = aws_eks_cluster.this.identity[0].oidc[0].issuer
+resource "aws_iam_role_policy_attachment" "node_group_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node_group.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_group_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node_group.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_group_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node_group.name
 }
--- 05-labs/play-ground/microservices/spring-petclinic-microservices/terraform/modules/eks/variables.tf
+++ 05-labs/play-ground/microservices/spring-petclinic-microservices/terraform/modules/eks/variables.tf
@@ -1,51 +1,14 @@
 variable "environment" {
-  type = string
-}
-
-variable "vpc_id" {
-  type = string
+  description = "Environment name"
+  type        = string
 }
 
 variable "private_subnet_ids" {
-  type = list(string)
+  description = "Private subnet IDs"
+  type        = list(string)
 }
 
 variable "cluster_version" {
-  type    = string
-  default = "1.31"
-}
-
-variable "desired_size" {
-  type    = number
-  default = 2
-}
-
-variable "max_size" {
-  type    = number
-  default = 4
-}
-
-variable "min_size" {
-  type    = number
-  default = 1
-}
-
-variable "instance_types" {
-  type    = list(string)
-  default = ["t3.medium"]
-}
-
-variable "capacity_type" {
-  type    = string
-  default = "ON_DEMAND"
-}
-
-variable "enable_fargate" {
-  type    = bool
-  default = false
-}
-
-variable "tags" {
-  type    = map(string)
-  default = {}
+  description = "EKS Cluster version"
+  type        = string
 }
