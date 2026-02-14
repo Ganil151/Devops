f946b67e13b1b630987e9653d0159d05409832fe
Ganil151 <ganilbatistyan@gmail.com>
2026-02-14 07:12:02 -0400

    refactor(Devops): Update manage-services.sh
    Changes summary:
    - Modified: 1 files
      • 07-boilerplates/04-playground/Block-PopUps/manage-services.sh

--- 07-boilerplates/04-playground/Block-PopUps/manage-services.sh
+++ 07-boilerplates/04-playground/Block-PopUps/manage-services.sh
@@ -9,8 +9,7 @@
 # ============================================================================
 # CONFIGURATION
 # ============================================================================
-BASE_DIR="home/gsmash/Documents/Devops/07-boilerplates/04-playground/Block-PopUps
-/block-popups"
+BASE_DIR="home/gsmash/Documents/Devops/07-boilerplates/04-playground/Block-PopUps/block-popups"
 PYTHON_SCRIPT="${BASE_DIR}/block-popups.py"
 SHELL_SCRIPT="${BASE_DIR}/block-popups.sh"
 
